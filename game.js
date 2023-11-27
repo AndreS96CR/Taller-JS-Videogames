d16531bcac4e2798592e682c94cdc040d5918b1a
Andres Gonzalez <and.96.03@gmail.com>
2023-11-27 08:51:25 -0600

    primera parte

---
+++ game.js
@@ -0,0 +1,44 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth){
+        canvasSize = innerWidth * 0.8;
+    }else{
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+    
+  elementSize = (canvasSize / 10) -4 ;
+
+  startGame();
+}
+
+function startGame() {
+
+    console.log(canvasSize, elementSize);
+
+
+    game.font = elementSize + 'px Verdana';
+    game.textAlign = 'center';
+
+    const map = maps[0];
+    const mapRows = maps[0].trim().split('\n');
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+    console.log(maps, mapRows, mapRowCols);
+
+      for (let row = 1; row <= 10; row++) {
+         for (let col = 1; col <= 10; col++) {
+             game.fillText(emojis['X'], elementSize * col, elementSize * row);
+         }
+      }
+
+}
